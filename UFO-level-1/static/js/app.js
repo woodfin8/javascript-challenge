2bcacfc2109a8340865c87edd49d1d1eefccfad2
woodfin8 <53791592+woodfin8@users.noreply.github.com>
2019-11-19 19:44:15 -0500

    UFO1 complete

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,42 @@
 // from data.js
 var tableData = data;
+var tbody = d3.select("tbody");
 
-// YOUR CODE HERE!
+
+// loop through list of objects in data.js and return value for each key 
+tableData.forEach((sighting)=>{
+    var row = tbody.append("tr");
+    Object.entries(sighting).forEach(([key,value])=>{
+        var cell = row.append("td");
+        cell.text(value);
+    });
+});
+
+
+
+//get button and input references
+var button = d3.select("#filter-btn");
+var input = d3.select("#datetime");
+
+
+//create funcition that clears the tbody and will create table with data filtered by datetime
+function searchDate(file) {
+    tbody.html("");
+    file.forEach((sighting)=>{
+        var row = tbody.append("tr");
+        Object.entries(sighting).forEach(([key,value])=>{
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
+
+}
+//write function for click event
+button.on("click", function() {
+    var newDate = input.property("value");
+    var filteredUFO = data.filter(saucer => saucer.datetime === newDate)
+    searchDate(filteredUFO)
+});
+
+
+
